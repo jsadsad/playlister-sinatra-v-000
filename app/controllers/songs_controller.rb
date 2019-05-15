ef88d4a4a305438b65746d7e4a130d60d109ed95
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-15 21:38:46 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,5 +17,5 @@ class SongsController < ApplicationController
   post '/songs' do
     @song = Song.create(params["Name"])
     @song.artist= Artist.find_or_create_by(:name => params["Artist Name"])
-    
+    song.
 end
